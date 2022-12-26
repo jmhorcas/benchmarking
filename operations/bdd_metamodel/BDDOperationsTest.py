341e0206248fedd5819741af9b820059e4afd7b7
José Miguel Horcas <horcas@uma.es>
2022-12-26 16:58:26 +0100

    Update BDD tests

--- operations/bdd_metamodel/BDDOperationsTest.py
+++ operations/bdd_metamodel/BDDOperationsTest.py
@@ -1,28 +1,33 @@
 import pytest
 
-from famapy.metamodels.fm_metamodel.transformations.featureide_reader import FeatureIDEReader
-from famapy.metamodels.bdd_metamodel.transformations.fm_to_bdd import FmToBDD
-from famapy.metamodels.bdd_metamodel.operations import (
+from flamapy.metamodels.fm_metamodel.transformations import UVLReader
+from flamapy.metamodels.bdd_metamodel.transformations import FmToBDD
+from flamapy.metamodels.bdd_metamodel.operations import (
     BDDProductsNumber,
     BDDProductDistribution
 )
 
 from models.models_info import *
 
+import sys
 
-def get_model(model_name):
-    fm = FeatureIDEReader(INPUT_FIDE_MODELS_FOLDER + model_name + FIDE_EXTENSION).transform()
-    bdd = FmToBDD(fm).transform()
-    return bdd
+sys.setrecursionlimit(5000)
 
-@pytest.mark.parametrize("model_name, expected_nof_products", [[m[NAME], m[NOF_PRODUCTS]] for m in MODELS[:8]])
+@pytest.mark.parametrize("model_name, expected_nof_products", [[m[NAME], m[NOF_PRODUCTS]] for m in MODELS[:9]])
 def test_nof_products(model_name, expected_nof_products):
-    model = get_model(model_name)
-    nof_products = BDDProductsNumber().execute(model).get_result()
+    fm = UVLReader(INPUT_UVL_MODELS_FOLDER + model_name + UVL_EXTENSION).transform()
+    bdd = FmToBDD(fm).transform()
+    
+    nof_products = BDDProductsNumber().execute(bdd).get_result()
     assert nof_products == expected_nof_products
 
-@pytest.mark.parametrize("model_name, expected_product_dist", [[m[NAME], m[PRODUCT_DISTRIBUTION]] for m in MODELS[:6]])
-def test_product_distribution(model_name, expected_product_dist):
-    model = get_model(model_name)
-    product_dist = BDDProductDistribution().execute(model).get_result()
-    assert product_dist == expected_product_dist
+@pytest.mark.parametrize("model_name", [m[NAME] for m in MODELS[:10]])
+def test_product_distribution(model_name):
+    fm = UVLReader(INPUT_UVL_MODELS_FOLDER + model_name + UVL_EXTENSION).transform()
+    bdd = FmToBDD(fm).transform()
+
+    product_dist = BDDProductDistribution().execute(bdd).get_result()
+    nof_products = BDDProductsNumber().execute(bdd).get_result()
+
+    assert len(product_dist) == len(fm.get_features())
+    assert sum(product_dist) == nof_products
